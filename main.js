f61cc1c9de514517d2f9982a0c0ee775a04f0265
Sarah Salmean <sarahsalmean@hotmail.com>
2023-03-24 11:49:17 +0000

    commented out quote changer

--- main.js
+++ main.js
@@ -58,8 +58,8 @@ async function fetchTrivia () {
 //this gives us the question.
     console.log(triviaData.results[0].question);
     let uncodedQuestion = triviaData.results[0].question;
-    //uncodedQuestion = uncodedQuestion.replace("&#039;"/g, "'");
-    //uncodedQuestion = uncodedQuestion.replace('&quot;'/g',`"`);
+    //uncodedQuestion = uncodedQuestion.replace("&#039;"/g, "\'");
+    //uncodedQuestion = uncodedQuestion.replace("&quot;"/g,`\"`);
     question.textContent = uncodedQuestion;
 }
 fetchTrivia();
